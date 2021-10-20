26f21e02c28603e39585a0ce8d8bdba887e9579a
Kristoffer Kjelde <kristoffer@kjelde.io>
2021-10-20 20:30:18 +0200

    feat: optimize elements - wip

--- src/index.js
+++ src/index.js
@@ -27,17 +27,24 @@ const Splitting = require("splitting");
     return;
   }
 
-  let _sideBySidelistener = function() {
+
+  let sideBySidelistener = function() {
     const sideBySideElement = document.querySelector('.side-by-side')
+    const image = sideBySideElement.querySelector('.visual-block__image');
 
     lastKnownScrollPosition = window.scrollY;
 
-    if (!ticking) {
-      window.requestAnimationFrame(function() {
-        hideArrow(sideBySideElement, lastKnownScrollPosition);
-        ticking = false;
-      });
-      ticking = true;
+    let repeatOften = function() {
+      hideArrow(sideBySideElement, lastKnownScrollPosition);
+      animateImage(sideBySideElement, image, lastKnownScrollPosition)
+
+      if (ticking) {
+        window.requestAnimationFrame(repeatOften)
+      }
+    };
+
+    if (ticking) {
+      window.requestAnimationFrame(repeatOften)
     }
   }
 
@@ -51,17 +58,64 @@ const Splitting = require("splitting");
     }
   }
 
+  function animateImage(sideBySideElement, image, lastKnownScrollPosition) {
+    let current = 0;
+    let ease = 0.75;
+    let containerTop = sideBySideElement.offsetTop;
+    let containerBottom = containerTop + image.getBoundingClientRect().height;
+
+    let target = (lastKnownScrollPosition - containerTop) / (containerBottom);
+    current = parseFloat(lerp(current, target, ease)).toFixed(2);
+    let scale = current / 4 + 1;
+    scale = scale < 1 ? 1 : scale;
+    scale = scale > 1.5 ? 1.5 : scale;
+
+    setTransform(image, `scale(${scale.toFixed(2)})`);
+  }
+
+  function lerp(start, end, t) {
+    return start * (1 - t) + end * t;
+  }
+
+  function setTransform(el, transform) {
+    el.style.transform = transform;
+  }
+
+  // Side By Side Module Observer - To determine when cta button is visible
+  const sideBySideObserverCallback = (entries) => {
+    ticking = entries.some(entry => entry.isIntersecting)
+
+    entries.forEach((entry) => {
+
+      if (entry.isIntersecting) {
+        document.addEventListener('scroll', sideBySidelistener, true);
+      } else {
+        document.removeEventListener('scroll', sideBySidelistener, true)
+      }
+    });
+  };
+  document.querySelectorAll(".side-by-side").forEach((i) => {
+    if (i) {
+      const observer = new IntersectionObserver(
+          (entries) => {
+            sideBySideObserverCallback(entries, observer, i);
+          },
+      );
+      observer.observe(i);
+    }
+  });
+
 
   /*document.querySelectorAll(querySelector).forEach((i) => {
     if (i) {
       const observer = new IntersectionObserver(
-        (entries) => {
-          Splitting({
-            by: "lines",
-          });
-          observerCallback(entries, observer, i);
-        },
-        { threshold: 0, rootMargin: "0px" }
+          (entries) => {
+            Splitting({
+              by: "lines",
+            });
+            observerCallback(entries, observer, i);
+          },
+          { threshold: 0, rootMargin: "0px" }
       );
       observer.observe(i);
     }
@@ -78,26 +132,8 @@ const Splitting = require("splitting");
 
   /*************************************************************/
 
-  // Side By Side Module Observer - To determine when cta button is visible
-  const sideBySideObserverCallback = (entries) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        document.addEventListener('scroll', _sideBySidelistener, true);
-      } else {
-        document.removeEventListener('scroll', _sideBySidelistener, true)
-      }
-    });
-  };
-  document.querySelectorAll(".side-by-side").forEach((i) => {
-    if (i) {
-      const observer = new IntersectionObserver(
-        (entries) => {
-          sideBySideObserverCallback(entries, observer, i);
-        },
-      );
-      observer.observe(i);
-    }
-  });
+
+
   /***************************** Circle Spinner Module Observer ********************************/
 
   /*const circleObserverCallback = (entries, observer) => {
@@ -138,7 +174,7 @@ const Splitting = require("splitting");
         //now add the strok dash array for the first two values
         //TODO : could this all be done with css?
         el.querySelector(".percent-circle-inner").style.strokeDasharray =
-          stroke + "px " + diff + "px";
+            stroke + "px " + diff + "px";
 
         observer.unobserve(el);
       }
@@ -148,10 +184,10 @@ const Splitting = require("splitting");
   document.querySelectorAll(".percent-circle").forEach((i) => {
     if (i) {
       const observer = new IntersectionObserver(
-        (entries) => {
-          circleObserverCallback(entries, observer, i);
-        },
-        { threshold: 0.5 }
+          (entries) => {
+            circleObserverCallback(entries, observer, i);
+          },
+          { threshold: 0.5 }
       );
       observer.observe(i);
     }
@@ -159,52 +195,16 @@ const Splitting = require("splitting");
 
 })();
 
-/*/!****************************** Side by side Top Module scaaling effect ******************************************!/
 
-let image = document.querySelector(".side-by-side .visual-block__image");
-let container = document.querySelector(".side-by-side");
-let containerTop = 0;
-let containerBottom = 0;
-let current = 0;
-let target = 0;
-let ease = 0.75;
+/************************** Small paralax effect on text container ************************************/
 
-function lerp(start, end, t) {
-  return start * (1 - t) + end * t;
-}
-
-function setTransform(el, transform) {
-  el.style.transform = transform;
-}
-
-function init() {
-  containerTop = container.offsetTop;
-  containerBottom = containerTop + image.getBoundingClientRect().height;
-}
-
-function animate() {
-  current = parseFloat(lerp(current, target, ease)).toFixed(2);
-  target = (window.scrollY - container.offsetTop) / containerBottom;
-
-  let scale = current / 4 + 1;
-  scale = scale < 1 ? 1 : scale;
-  scale = scale > 1.5 ? 1.5 : scale;
-  setTransform(image, `scale(${scale})`);
-  requestAnimationFrame(animate);
-}
-
-init();
-animate();
-
-/!*************************** Small paralax effect on text container ************************************!/
-
-document.querySelectorAll(".hero-a").forEach((i) => {
+/*document.querySelectorAll(".hero-a").forEach((i) => {
   if (i) {
     const observer = new IntersectionObserver(
-      (entries) => {
-        heroObserverCallback(entries, observer, i);
-      },
-      { threshold: 0 }
+        (entries) => {
+          heroObserverCallback(entries, observer, i);
+        },
+        { threshold: 0 }
     );
     observer.observe(i);
   }
@@ -218,11 +218,11 @@ const heroObserverCallback = (entries, observer) => {
       const scale = entry.target.querySelector(".background");
       window.addEventListener("scroll", function () {
         let offset =
-          entry.target.clientHeight - (entry.target.offsetTop - window.scrollY);
+            entry.target.clientHeight - (entry.target.offsetTop - window.scrollY);
 
         const viewPortWidth = Math.max(
-          document.documentElement.clientWidth || 0,
-          window.innerWidth || 0
+            document.documentElement.clientWidth || 0,
+            window.innerWidth || 0
         );
         let changeScale = offset * 0.001 + 1;
 
